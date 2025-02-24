5dd0a0ebd9b47f0a0f6147f51e04fa930fe445b7
jun.cai <jun.cai@suwell.cn>
2025-02-24 15:27:14 +0800

    修正

--- run.sh
+++ run.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# 定义帮助信息函数
+print_help() {
+  echo "使用方法: $0 [-b] [-a] [-r [build|<container_name>]]"
+  echo "选项："
+  echo "  -b    构建基础镜像"
+  echo "  -a    构建应用镜像"
+  echo "  -r    运行容器"
+  echo "        - build: 构建并运行容器"
+  echo "        - <container_name>: 运行指定容器"
+  exit 1
+}
+
 # 获取完整的命令行
 MAIN_DIR=$(dirname "$(readlink -f "$0")")
 env_file="${MAIN_DIR}/.env"
@@ -9,78 +21,68 @@ if [ -f ${env_file} ]; then
   source ${env_file}
 fi
 
-while getopts "bard:" opt; do
+# 如果没有参数，显示帮助信息
+if [ $# -eq 0 ]; then
+  print_help
+fi
+
+while getopts "bar:h" opt; do
   case $opt in
-    r)
-        shift $((OPTIND-1))
-        param1=$1
+  r)
+    param1=$OPTARG
+    echo ${param1}
 
-        if [ "$param1" = "build" ]; then
-            CMD="docker-compose --env-file ${env_file} -f docker/docker-compose.yaml --project-directory ${MAIN_DIR} up --build"
-        else
-            CMD="docker-compose --env-file ${env_file} -f docker/docker-compose.yaml up $param1"
-        fi
-      ;;
+    if [ -z "$param1" ]; then
+      echo "错误: -r 选项需要参数" >&2
+      print_help
+    fi
 
+    if [ "$param1" = "build" ]; then
+      CMD="docker-compose --env-file ${env_file} -f docker/docker-compose.yaml --project-directory ${MAIN_DIR} up --build"
+    else
+      CMD="docker-compose --env-file ${env_file} -f docker/docker-compose.yaml up $param1"
+    fi
+    ;;
 
-    b)
-        echo "构建基础镜像"
-        CMD="docker build -t ${IMAGE_NAME} . \
-                -f docker/dockerfile \
-                --build-arg BASE_IMAGE=${BASE_IMAGE} \
-                --build-arg RUNNER_IMAGE=${RUNNER_IMAGE}
-                "
-      ;;
+  b)
+    echo "构建基础镜像"
+    CMD="docker build -t ${IMAGE_NAME} . \
+        -f docker/dockerfile \
+        --build-arg BASE_IMAGE=${BASE_IMAGE} \
+        --build-arg RUNNER_IMAGE=${RUNNER_IMAGE}
+        "
+    ;;
 
-    a)
-        echo "构建应用镜像"
+  a)
+    echo "构建应用镜像"
+    echo "GIT用户名:"
+    read username
+    echo "GIT密码:"
+    read -s password
 
-        # 提示用户输入用户名
-        echo "GIT用户名:"
-        read username
+    CMD="docker build -t ${IMAGE_NAME}_app . \
+        -f docker/dockerfile_app \
+        --build-arg RUNNER_IMAGE=${IMAGE_NAME} \
+        --build-arg GIT_USERNAME=${username} \
+        --build-arg GIT_PASSWORD=${password}"
+    ;;
 
-        # 提示用户输入密码
-        echo "GIT密码:"
-        # -s 选项使得输入时不回显
-        read -s password
+  h)
+    print_help
+    ;;
 
-        CMD="docker build -t ${IMAGE_NAME}_app . \
-                -f docker/dockerfile_app \
-                --build-arg RUNNER_IMAGE=${IMAGE_NAME} \
-                --build-arg GIT_USERNAME=${username} \
-                --build-arg GIT_PASSWORD=${password}"
-      ;;
-
-    # r)
-    #     echo "构建应用容器"
-    #     docker rm -f ${CONTAINER_NAME}_app
-
-    #     # 构建容器
-    #     CMD="docker run -d --name ${CONTAINER_NAME}_app \
-    #                 -v /etc/localtime:/etc/localtime:ro \
-    #                 -v /root/data/:/root/data \
-    #                 -w /root/app \
-    #                 -p ${WEB_START_PORT}:80 \
-    #                 -p ${SANDBOX_START_PORT}:8194 \
-    #                 -e API_KEY=${SANDBOX_API_KEY:-dify-sandbox} \
-    #                 -e GIN_MODE=${SANDBOX_GIN_MODE:-release} \
-    #                 -e WORKER_TIMEOUT=${SANDBOX_WORKER_TIMEOUT:-15} \
-    #                 -e ENABLE_NETWORK=${SANDBOX_ENABLE_NETWORK:-true} \
-    #                 -e HTTP_PROXY=${SANDBOX_HTTP_PROXY:-http://ssrf_proxy:3128} \
-    #                 -e HTTPS_PROXY=${SANDBOX_HTTPS_PROXY:-http://ssrf_proxy:3128} \
-    #                 -e SANDBOX_PORT=${SANDBOX_PORT:-8194} \
-    #                 ${IMAGE_NAME} \
-    #                 "
-    #                 # tail -f /dev/null
-    #                 # --gpus all \
-    #     ;;
-
-    \?)
-      echo "无效的选项: -$OPTARG" >&2
-      exit 1
-      ;;
+  \?)
+    echo "错误: 无效的选项 -$OPTARG" >&2
+    print_help
+    ;;
   esac
 done
+
+# 检查是否设置了CMD
+if [ -z "$CMD" ]; then
+  echo "错误: 无效的参数组合" >&2
+  print_help
+fi
 
 echo ${CMD}
 ( ${CMD} )
