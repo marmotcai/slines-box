e2f54527881ecb1a63a9f4ca7f4cc680587253f3
caijun <caijun@atoml.com>
2025-04-16 11:25:02 +0800

    同步

---
+++ ollama/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+if [[ "${MODE}" == "keep" ]]; then
+  echo "Starting keep debug process"
+  tail -f /dev/null
+else
+  echo "Starting in ${MODE} mode"
+
+  ollama serve
+fi
